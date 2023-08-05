6567afb1523d049611cce53e04b731e257b10aa8
Nikola <n.tsvetkov9@gmail.com>
2023-08-05 14:58:18 +0300

    Updating React version from 16 to 18

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
-);
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
+
+// React 16 way:
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+// 	<React.StrictMode>
+// 		<App />
+// 	</React.StrictMode>,
+// 	document.getElementById("root")
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
