cd133d0fe07cefa70393bf7b6fda0349449d26c9
Nikola <n.tsvetkov9@gmail.com>
2023-08-15 09:17:01 +0300

    Fixing formatting in craco config

--- craco.config.js
+++ craco.config.js
@@ -1,17 +1,6 @@
 const CracoLessPlugin = require("craco-less");
 
 module.exports = {
-	// module: {
-	// 	rules: [
-	// 		{
-	// 			test: /\.(png|jpg|svg|gif)?$/,
-	// 			use: {
-	// 				loader: 'url-loader',
-	// 				options: { sourceMap: true }
-	// 			},
-	// 		}
-	// 	]
-	// },
 	plugins: [
 		{
 			plugin: CracoLessPlugin,
