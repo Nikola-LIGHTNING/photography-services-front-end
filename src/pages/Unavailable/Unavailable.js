77bacc4ef44df7be6e598ae27cade35cd1473c81
Nikola <n.tsvetkov9@gmail.com>
2025-07-20 15:30:39 +0300

    Feature(unavailable): enhance unavailable page with home button and styling
    Improves the user experience on the 'Unavailable' page by adding a 'Go to Home' button, providing a clear path for users to navigate back to the application's home page.
    
    - Adds a 'Go to Home' button using Ant Design components.
    
    - Updates the stylesheet to center the page content and manage the layout of the message and button.
    
    - Increases the font size of the unavailable message for better visibility.

--- src/pages/Unavailable/Unavailable.js
+++ src/pages/Unavailable/Unavailable.js
@@ -1,9 +1,18 @@
-import "./Unavailable.less";
+import { Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import './Unavailable.less';
 
 function Unavailable() {
+	const navigate = useNavigate();
+
 	return (
 		<div className="unavailableContainer">
-			<div className="unavailableMessage">Oops, something went wrong. Please, try again later!</div>
+			<div className="unavailableContent">
+				<div className="unavailableMessage">Oops, something went wrong. Please, try again later!</div>
+				<Button type="primary" onClick={() => navigate('/')}>
+					Go to Home
+				</Button>
+			</div>
 		</div>
 	);
 }
